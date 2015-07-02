0c49d960542051178c46d5a7385e2160690b5cdd
Alberto97 <albertop2197@gmail.com>
2015-07-02 18:30:39 +0200

    falcon: Update XT1032, XT1033 and XT1034 fingerprint
    Change-Id: If0d4818392c87fc2c02258ee65cb42ef0467be2e

--- init/init_falcon.c
+++ init/init_falcon.c
@@ -72,10 +72,10 @@ void vendor_load_properties()
         } else {
             /* xt1032 */
             property_set("ro.product.device", "falcon_umts");
-            property_set("ro.build.description", "falcon_retgb-user 5.0.2 LXB22.46-28.1 1 release-keys");
-            property_set("ro.build.fingerprint", "motorola/falcon_retgb/falcon_umts:5.0.2/LXB22.46-28.1/1:user/release-keys");
+            property_set("ro.build.description", "falcon_retuglb-user 5.0.2 LXB22.99-36 35 release-keys");
+            property_set("ro.build.fingerprint", "motorola/falcon_retuglb/falcon_umts:5.0.2/LXB22.99-36/35:user/release-keys");
             property_set("ro.build.product", "falcon_umts");
-            property_set("ro.mot.build.customerid", "RTGB");
+            property_set("ro.mot.build.customerid", "retusa_glb");
             property_set("ro.telephony.default_network", "0");
             property_set("persist.radio.multisim.config", "");
         }
@@ -119,8 +119,8 @@ void vendor_load_properties()
     } else if (ISMATCH(radio, "0x5")) {
         /* xt1033 */
         property_set("ro.product.device", "falcon_umtsds");
-        property_set("ro.build.description", "falcon_retbr_ds-user 5.0 LXB22.46-16 16 release-keys");
-        property_set("ro.build.fingerprint", "motorola/falcon_retbr_ds/falcon_umtsds:5.0/LXB22.46-16/16:user/release-keys");
+        property_set("ro.build.description", "falcon_retbr_ds-user 5.0.2 LXB22.46-28 27 release-keys");
+        property_set("ro.build.fingerprint", "motorola/falcon_retbr_ds/falcon_umtsds:5.0.2/LXB22.46-28/27:user/release-keys");
         property_set("ro.build.product", "falcon_umtsds");
         property_set("ro.mot.build.customerid", "RETBR");
         property_set("ro.telephony.default_network", "0,1");
@@ -130,8 +130,8 @@ void vendor_load_properties()
     } else if (ISMATCH(radio, "0x6")) {
         /* xt1034 */
         property_set("ro.product.device", "falcon_umts");
-        property_set("ro.build.description", "falcon_retuaws-user 4.4.3 KXB21.14-L1.32 30 release-keys");
-        property_set("ro.build.fingerprint", "motorola/falcon_retuaws/falcon_umts:4.4.3/KXB21.14-L1.32/30:user/release-keys");
+        property_set("ro.build.description", "falcon_retuaws-user 5.0.2 LXB22.99-36 19 release-keys");
+        property_set("ro.build.fingerprint", "motorola/falcon_retuaws/falcon_umts:5.0.2/LXB22.99-36/19:user/release-keys");
         property_set("ro.build.product", "falcon_umts");
         property_set("ro.mot.build.customerid", "retusa_aws");
         property_set("ro.telephony.default_network", "0");
